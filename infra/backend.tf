a85299b8ad1670a76ccb4f96563d0cdb4ebc30d8
xEzIxX <by1094@gachon.ac.kr>
2026-09-17 13:35:48 +0900

    feat: Terraform 상태 관리를 위한 S3 백엔드 설정 추가
    - 테라폼은 기본적으로 terraform.tfstate를 로컬에 저장하지만, 공동 작업자가 존재
    - 공식문서에서 권장하는 방식 's3에 state를 저장'하도록 설정

---
+++ infra/backend.tf
@@ -0,0 +1,13 @@
+# Terraform 상태 파일(tfstate)을 S3에 저장하여
+# 팀원 간 동일한 인프라 상태를 공유하고, 동시 변경 시 충돌을 방지한다.
+# Terraform 공식 문서 : Backend = Terraform state를 어디에 저장하고 어떻게 잠글지 결정하는 구성
+
+terraform {
+  backend "s3" {
+    bucket       = "stockspoon-terraform-state-v1"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-northeast-2"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
