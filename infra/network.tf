913ad467e61f665ed72b4260502b3861d252d270
xEzIxX <by1094@gachon.ac.kr>
2026-09-17 14:04:45 +0900

    feat: 퍼블릭 서브넷 인터넷 라우팅 구성 추가
    - Internet Gateway 생성 및 VPC 연결
    - 퍼블릭 서브넷용 Route Table 생성
    - 0.0.0.0/0 트래픽을 Internet Gateway로 전달하도록 라우트 설정
    - 퍼블릭 서브넷과 Route Table 연결

--- infra/network.tf
+++ infra/network.tf
@@ -20,3 +20,34 @@ resource "aws_subnet" "public" {
     Name = "stockspoon-public-subnet"
   }
 }
+
+# VPC와 인터넷 간 통신을 위한 Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "stockspoon-igw"
+  }
+}
+
+# Public Subnet의 트래픽 경로를 관리하는 Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "stockspoon-public-rt"
+  }
+}
+
+# 인터넷으로 향하는 모든 IPv4 트래픽을 Internet Gateway로 전달
+resource "aws_route" "internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# Public Subnet에 Public Route Table 연결
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
